5140b00536adeea0a8f560d0b422811cf01db0fa
Ravinder <infitake@.github.com>
2018-12-11 15:01:27 +0530

    complete Route

--- poketimes/src/components/Navbar.js
+++ poketimes/src/components/Navbar.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { Link, NavLink,withRouter} from 'react-router-dom'
 const Navbar = (props) => {
     //this is programmatic redirects using the withRouter which act as the function
-    setTimeout(() => {
-        props.history.push('/')
-      },5000);
+    //setTimeout(() => {
+    //    props.history.push('/')
+    //  },5000);
     return (
         <nav className="nav-wrapper red darken-3">
             <div className="container">
